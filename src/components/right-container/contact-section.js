3e0ff74a27057cec3f535b808761d46fe2a0b2bc
prathamnemade <prathameshnemade95@gmail.com>
2021-08-09 12:41:01 +0530

    mobile CSS

---
+++ src/components/right-container/contact-section.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ContactSection = () => {
+    return <div id="contact-section" className="common-section align-center" >
+        <div className="heading">CONTACT</div>
+        <div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>
+}
+
+export default ContactSection
--- src/components/right-container/index.js
+++ src/components/right-container/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import AboutSection from './about-section'
+import ContactSection from './contact-section'
 import HomeSection from './home-section'
 import './style.scss'
 
@@ -8,7 +9,7 @@ const InformationContainer = () => {
         <div className="information-box">
             <HomeSection />
             <AboutSection />
-            <div className="common-section">dff</div>
+            <ContactSection/>
         </div>
     )
 }
