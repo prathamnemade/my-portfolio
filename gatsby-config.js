8c7fdc4536c22f4612c2619364bbe4257f7abf80
Prathamesh Nemade <30521872+prathamnemade@users.noreply.github.com>
2024-02-25 21:08:04 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://friendly-dijkstra-a75b54.netlify.app/",
     title: "Prathamesh Nemade",
   },
   plugins: [
