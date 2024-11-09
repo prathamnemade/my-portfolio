ddeff9f4c758931a648801dbab71a6208ad1fbdd
Prathamesh Nemade <prathameshnemade95@gmail.com>
2024-11-09 12:56:32 -0500

    added seo component

--- src/components/right-container/index.js
+++ src/components/right-container/index.js
@@ -1,19 +1,21 @@
-import React from 'react'
-import AboutSection from './about-section'
-import ContactSection from './contact-section'
-import HomeSection from './home-section'
-import PortfolioSection from './portfolio-section'
-import './style.scss'
+import React from "react";
+import AboutSection from "./about-section";
+import ContactSection from "./contact-section";
+import HomeSection from "./home-section";
+import PortfolioSection from "./portfolio-section";
+import SEO from "./seo";
+import "./style.scss";
 
 const InformationContainer = () => {
-    return (
-        <div className="information-box">
-            <HomeSection />
-            <AboutSection />
-            <PortfolioSection />
-            <ContactSection />
-        </div>
-    )
-}
+  return (
+    <div className="information-box">
+      <SEO />
+      <HomeSection />
+      <AboutSection />
+      <PortfolioSection />
+      <ContactSection />
+    </div>
+  );
+};
 
-export default InformationContainer
+export default InformationContainer;
--- src/components/sharing-container/index.js
+++ src/components/sharing-container/index.js
@@ -1,36 +1,50 @@
-import React from 'react'
-import { SocialMediaIconsReact } from 'social-media-icons-react';
-import './style.scss'
+import React from "react";
+import { SocialIcon } from "react-social-icons";
+import "./style.scss";
 
 const OPTIONS = [
-    { name: "Facebook", icon: "facebook", url: "https://www.facebook.com/prathamesh.nemade.9" },
-    { name: "Instagram", icon: "instagram", url: "https://www.instagram.com/itsprathameshsfault/" },
-    {
-        name: "Snapchat", icon: "snapchat", url: "https://www.snapchat.com/add/prathamn?share_id=RTBCMjU5&locale=en_IN"
-    },
-    { name: "GitHub", icon: "github", url: "https://github.com/prathamnemade" },
-    { name: "LinkedIn", icon: "linkedin", url: "https://www.linkedin.com/in/prathamnemade/" },
-    { name: "Medium", icon: "medium", url: "https://prathameshnemade.medium.com/about" },
-    { name: "Mail", icon: "mail", url: "mailto:prathameshnemade95@gmail.com" },
-]
+  {
+    name: "Facebook",
+    icon: "facebook",
+    url: "https://www.facebook.com/prathamesh.nemade.9",
+  },
+  {
+    name: "Instagram",
+    icon: "instagram",
+    url: "https://www.instagram.com/itsprathameshsfault/",
+  },
+  {
+    name: "Snapchat",
+    icon: "snapchat",
+    url: "https://www.snapchat.com/add/prathamn?share_id=RTBCMjU5&locale=en_IN",
+  },
+  { name: "GitHub", icon: "github", url: "https://github.com/prathamnemade" },
+  {
+    name: "LinkedIn",
+    icon: "linkedin",
+    url: "https://www.linkedin.com/in/prathamnemade/",
+  },
+  {
+    name: "Medium",
+    icon: "medium",
+    url: "https://prathameshnemade.medium.com/about",
+  },
+  { name: "Mail", icon: "mail", url: "mailto:prathameshnemade95@gmail.com" },
+];
 
 const SharingComponent = () => {
-    return (
-        <div className="sharing-container">
-            {OPTIONS && OPTIONS.map((option, index) =>
-                <SocialMediaIconsReact key={'icon' + index}
-                    borderWidth={"1"}
-                    borderColor="#c2cad7"
-                    icon={option.icon}
-                    iconColor="#58595b"
-                    iconSize={"0"}
-                    backgroundColor="#FFF"
-                    roundness="50%"
-                    size="30"
-                    url={option.url} />
-            )}
-        </div>
-    )
-}
+  return (
+    <div className="sharing-container">
+      {OPTIONS &&
+        OPTIONS.map((option, index) => (
+          <SocialIcon
+            key={"icon" + index}
+            icon={option.icon}
+            url={option.url}
+          />
+        ))}
+    </div>
+  );
+};
 
-export default SharingComponent
+export default SharingComponent;
